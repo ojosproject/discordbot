5e260e48caa7a2b6593362032d2e109a9e2e1697
Carlos Valdez <carlos@blackswan3d.com>
2023-09-12 23:22:48 -0700

    created a constant of the server id

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ intents.presences = True
 intents.members = True
 
 
+BS3D_GUID_ID = 1128184237535805442
+
+
 class ChaluBot(discord.Client):
     def __init__(self, *, intents: discord.Intents):
         super().__init__(intents=intents)
@@ -18,7 +21,7 @@ class ChaluBot(discord.Client):
         print("Ready to go!")
 
     async def setup_hook(self):
-        await self.tree.sync(guild=discord.Object(id=1128184237535805442))
+        await self.tree.sync(guild=discord.Object(id=BS3D_GUID_ID))
 
 
 # Commands begin below.
@@ -26,7 +29,7 @@ client = ChaluBot(intents=intents)
 
 @client.tree.command(name="ticket",
                      description="Want to report a bug or request a feature? Use this command!",
-                     guilds=[discord.Object(id=1128184237535805442)])
+                     guilds=[discord.Object(id=BS3D_GUID_ID)])
 async def new_issue(intraction: discord.Interaction):
     ...
     
