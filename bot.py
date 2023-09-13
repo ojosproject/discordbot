f312d535ee67af5b3fc375e40313beba3023c390
Carlos Valdez <carlos@blackswan3d.com>
2023-09-12 23:37:15 -0700

    feat: added description and choices to ticket

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord import app_commands
+from typing import Literal
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -30,7 +31,11 @@ client = ChaluBot(intents=intents)
 @client.tree.command(name="ticket",
                      description="Want to report a bug or request a feature? Use this command!",
                      guilds=[discord.Object(id=BS3D_GUID_ID)])
-async def new_issue(intraction: discord.Interaction):
+@app_commands.describe(label="Is this a BUG or a FEATURE?",
+                       request="What feature/bug fix would you like?",
+                       page="What page should this feature/bug fix be on?",
+                       deadline="Please specify a deadline for this feature/bug fix to be done.")
+async def new_issue(intraction: discord.Interaction, label: Literal["bug", "feature"], request: str, page: Literal["/about/", "/showcase/", "/our-team/", "All pages"], deadline: str):
     ...
     
 
