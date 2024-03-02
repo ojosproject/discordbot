9672ffa518a514e01b7d377efeee88af6328d6a6
Carlos Valdez <cvaldezh@uci.edu>
2024-03-01 21:25:40 -0800

    docs(data): added docstrings

--- src/data.py
+++ src/data.py
@@ -27,6 +27,14 @@ class Data:
     _content: dict
 
     def __init__(self, file: str):
+        """Initiates the Data class
+
+        Args:
+            file (str): The file that has data to use/change
+
+        Raises:
+            FileNotAvailableError: Raised if the file does not exist
+        """
         if not Path(file).exists():
             raise FileNotAvailableError("Data.__init__: The file was not found on the server.")
     
@@ -34,22 +42,42 @@ class Data:
         self._content = json.loads(self._file.read_text())
 
     def commit(self) -> None:
+        """
+        Saves the information in self._content to the self._file file.
+        """
         with open(self._file, 'w+') as f:
             f.write(json.dumps(self._content, indent=4))
 
     def _readings_already_included(self, url: str) -> int:
+        """Checks whether or not this is a duplicate reading with its URL.
+
+        Args:
+            url (str): The URL to search for
+
+        Returns:
+            int: Returns the index of the reading if it's included, -1 otherwise
+        """
         for index, reading in enumerate(self._content['readings']):
             if reading['url'] == url:
                 return index
         return -1
 
     def add_reading(self, title: str, url: str) -> None:
+        """Adds a reading to self._content
+
+        Args:
+            title (str): The title of the paper
+            url (str): The URL of the paper
+
+        Raises:
+            DuplicateReadingError: The reading is already included in self._content
+        """
         if self._readings_already_included(url) != -1:
             raise DuplicateReadingError('Data.add_reading: The reading is already included.')
 
         self._content['readings'].append(
             {
-                "id": time.time(),
+                "id": int(time.time()),
                 "title": title,
                 "url": url,
                 "assigned_to": 0,
@@ -60,6 +88,15 @@ class Data:
         )
 
     def assign_reading(self, url: str, user_id: int) -> None:
+        """Assigns a paper to an individual.
+
+        Args:
+            url (str): The URL that's waiting to be assigned
+            user_id (int): A Discord user ID to assign the reading to
+
+        Raises:
+            ReadingNotFoundError: Raised if the URL is not in the system.
+        """
         reading_index = self._readings_already_included(url)
         
         if reading_index == -1:
@@ -70,6 +107,17 @@ class Data:
         })
 
     def add_notes_and_summary(self, url: str, notes: str, summary: str) -> None:
+        """Adds notes and a summary to a paper.
+
+        Args:
+            url (str): The URL to add the notes to
+            notes (str): The notes
+            summary (str): A summary of the paper itself
+
+        Raises:
+            ReadingNotFoundError: Raised when the URL is not in the system
+            MissingAssigneeError: Raised when the referenced paper does not have an assignee
+        """
         reading_index = self._readings_already_included(url)
 
         if reading_index == -1:
@@ -84,6 +132,14 @@ class Data:
         })
 
     def git(self, url: str) -> None:
+        """Writes up instructions to eventually deliver to git
+
+        Args:
+            url (str): The URL that should be committed to the git repository
+
+        Raises:
+            MissingNotesError: Raised if the paper does not have notes or a summary
+        """
         reading_index = self._readings_already_included(url)
 
         data = self._content['readings'][reading_index]
