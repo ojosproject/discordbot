3e2507301623791b9634418895ca22a153c8c558
Carlos Valdez <cvaldezh@uci.edu>
2024-03-02 00:06:29 -0800

    feat: finished commands

--- src/bot.py
+++ src/bot.py
@@ -35,21 +35,61 @@ class Sauron(discord.Client):
             await self.tree.sync(guild=discord.Object(id=guild_id))
 
 
+# Models begin below.
+
+class Notes(discord.ui.Modal):
+    summary = discord.ui.TextInput(
+        label='Summary',
+        style=discord.TextStyle.long,
+        placeholder='Summary of the research paper...',
+    )
+
+    notes = discord.ui.TextInput(
+        label="Notes",
+        style=discord.TextStyle.long,
+        placeholder="Your notes in bullet points...",
+    )
+
+    def __init__(self, data: Data, paper_id: int):
+        super().__init__(title='Notes')
+        self._data = data
+        self._paper = data.get_paper(paper_id)
+
+        self.summary.default = self._paper['summary']
+        self.notes.default = self._paper['notes']
+
+
+    async def on_submit(self, interaction: discord.Interaction):
+        self._data.add_notes_and_summary(
+            url=self._paper['url'],
+            notes=self.notes.value,
+            summary=self.summary.value
+        )
+
+        self._data.commit()
+        await interaction.response.send_message(f"Done! Thank you for the notes, {interaction.user.mention}! If you think this is ready to publish, go ahead and try `/commit {self._paper['id']}`.\n\n__Your notes for [{self._paper['title']}]({self._paper['url']}):__\n\n{self._paper['notes']}\n\n__Your summary:__\n{self._paper['summary']}")
+
+
+
+
 # Commands begin below.
 client = Sauron(intents=intents)
 
-@client.tree.command(name="add",
+@client.tree.command(name="add_paper",
                     description="Add a research paper for later",
                     guilds=[discord.Object(id=server_id) for server_id in SERVER_WHITELIST])
 @app_commands.describe(
     title="What is the title of this paper?",
     url="What is the Google Drive URL?"
 )
-async def add(interaction: discord.Interaction, title: str, url: str):
+async def add_paper(interaction: discord.Interaction, title: str, url: str):
     try:
-        client.data.add_reading(title, url)
-        client.data.commit()
-        await interaction.response.send_message(f":white_check_mark: Done! Added [{title}]({url}). If you'd like to claim this reading, please use `/claim`!")
+        if OJOS_TEAM_ID in [x.id for x in interaction.user.roles]:
+            client.data.add_reading(title, url)
+            client.data.commit()
+            await interaction.response.send_message(f":white_check_mark: Done! Added [{title}]({url}). If you'd like to claim this reading, please use `/claim`!")
+        else:
+            await interaction.response.send_message(":x: Sorry, but you're not authorized to use this command.", ephemeral=True, delete_after=10)
     except DuplicateReadingError:
         await interaction.response.send_message(":x: Sorry, this article is already registered.", ephemeral=True, delete_after=10)
     except discord.HTTPException:
@@ -131,6 +171,7 @@ async def assign(interaction: discord.Interaction, member: discord.Member, paper
     except discord.HTTPException:
         await interaction.response.send_message("Sorry, I had an unexpected error. :/", delete_after=10)
 
+
 @client.tree.command(name="claim",
                     description="Claim a research paper.",
                     guilds=[discord.Object(id=server_id) for server_id in SERVER_WHITELIST])
@@ -139,15 +180,19 @@ async def assign(interaction: discord.Interaction, member: discord.Member, paper
 )
 async def claim(interaction: discord.Interaction, paper_id: int):
     try:
-        paper = client.data.get_paper(paper_id)
+        if OJOS_TEAM_ID in [x.id for x in interaction.user.roles]:
+            paper = client.data.get_paper(paper_id)
 
-        client.data.assign_reading(paper['url'], interaction.user.id)
-        client.data.commit()
-        await interaction.response.send_message(f"{interaction.user.mention}, you've been assigned the following reading: [{paper['title']}]({paper['url']})")
+            client.data.assign_reading(paper['url'], interaction.user.id)
+            client.data.commit()
+            await interaction.response.send_message(f"{interaction.user.mention}, you've been assigned the following reading: [{paper['title']}]({paper['url']})")
+        else:
+            await interaction.response.send_message(":x: Sorry, but you're not authorized to use this command.", ephemeral=True, delete_after=10)
     except ReadingNotFoundError:
         await interaction.response.send_message(":question: The Paper ID was not found in the system.", ephemeral=True, delete_after=10)
     except discord.HTTPException:
         await interaction.response.send_message("Sorry, I had an unexpected error. :/", delete_after=10)
+
 
 @client.tree.command(name="commit",
                     description="Finished your research? Commit it!",
@@ -159,15 +204,37 @@ async def commit(interaction: discord.Interaction, paper_id: int):
     try:
         paper = client.data.get_paper(paper_id)
 
-        client.data.git(
-            paper['url']
-        )
+        if interaction.user.id == paper['assigned_to']:
+            client.data.git(
+                paper['url']
+            )
 
-        await interaction.response.send_message(f"Your notes will be published soon once ${interaction.guild.get_member(CARLOS_ID).mention} gets on the computer. Thank you, ${interaction.user.mention}!")
+            await interaction.response.send_message(f"Your notes will be published soon once ${interaction.guild.get_member(CARLOS_ID).mention} gets on the computer. Thank you, ${interaction.user.mention}!")
+        else:
+            await interaction.response.send_message(":x: Sorry, but you're not assigned to this paper.", ephemeral=True, delete_after=10)
     except MissingNotesError:
         await interaction.response.send_message(f":x: You need to add both notes and summary to make public.", ephemeral=True, delete_after=10)
     except discord.HTTPException:
         await interaction.response.send_message("Sorry, I had an unexpected error. :/", delete_after=10)
+
+
+@client.tree.command(name="add_notes",
+                    description="Add notes to a research document",
+                    guilds=[discord.Object(id=server_id) for server_id in SERVER_WHITELIST])
+@app_commands.describe(
+    paper_id="The paper to add notes to."
+)
+async def add_notes(interaction: discord.Interaction, paper_id: int):
+    if interaction.user.id == client.data.get_paper(paper_id)['assigned_to']:
+        await interaction.response.send_modal(Notes(client.data, paper_id))
+    else:
+        await interaction.response.send_message(":x: Sorry, but you're not assigned to this paper.", ephemeral=True, delete_after=10)
+
+@client.tree.command(name="help",
+                    description="Gives you the link to the docs website.",
+                    guilds=[discord.Object(id=server_id) for server_id in SERVER_WHITELIST])
+async def send_help(interaction: discord.Interaction):
+    await interaction.response.send_message("https://docs.ojosproject.org/teams/research/chalubot")
 
 
 client.run(os.getenv("DISCORD_TOKEN"))
